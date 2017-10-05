3342e6dae6ce49c41eba67b6bd513072d9b228dc
eduardo.colin <eduardo.colin@kuspit.com>
2017-10-04 19:29:19 -0500

    Commit inicial

---
+++ src/main/java/com/legvit/pld/App.java
@@ -0,0 +1,22 @@
+package com.legvit.pld;
+
+/*import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;*/
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	
+	private static final String CONFIG_PATH = "classpath*:com/sw_engineering_candies/application-config.xml";
+	
+    public static void main( String[] args ) {
+        /*System.out.println( "Hello World!" );
+        final ApplicationContext context = new ClassPathXmlApplicationContext(CONFIG_PATH);
+        final MinimalSpringApp minimalSpringApp = context.getBean(MinimalSpringApp.class);
+        minimalSpringApp.sayHello();*/
+    }
+}
