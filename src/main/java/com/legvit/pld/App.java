f55f45bdc558bb0d822a0a9584cf472fb067979a
lalo <lalo@Colin_Guerrero>
2017-10-08 17:46:04 -0500

    * Modificaciones para PLD.

--- src/main/java/com/legvit/pld/App.java
+++ src/main/java/com/legvit/pld/App.java
@@ -1,24 +1,15 @@
 package com.legvit.pld;
 
-/*import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.stereotype.Component;*/
 
-/**
- * Hello world!
- *
- */
 public class App {
 	
-	private static final String CONFIG_PATH = "classpath*:com/sw_engineering_candies/application-config.xml";
+	private static final String[] CONFIG_PATH = new String[] {"com/legvit/pld/springconfig/applicationContext.xml", 
+															  "com/legvit/pld/springconfig/pld-batch-quartz.xml", 
+															  "com/legvit/pld/springconfig/pld-quartz.xml"};
 	
     public static void main( String[] args ) {
-        /*System.out.println( "Hello World!" );
         final ApplicationContext context = new ClassPathXmlApplicationContext(CONFIG_PATH);
-        final MinimalSpringApp minimalSpringApp = context.getBean(MinimalSpringApp.class);
-        minimalSpringApp.sayHello();*/
-    	
-    	System.out.println("Prueba");
     }
 }
